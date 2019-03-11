024e3785efe926012ac3fdeb20f30fd9cda756ad
Brianna Rice <brianna.rice@gulllakecs.org>
2019-03-11 12:39:47 -0400

    Add test final

---
+++ Final_Project.py
@@ -0,0 +1,9 @@
+# 3/11/19 Final Project
+# Brianna Rice
+
+print('Pick a number between 1 and 30')
+magic_number = 3
+guess = int(input('Enter a number:'))
+
+while guess != magic_number:
+    print
