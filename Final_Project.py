edf62751c379871093a604fb9d78360043f1d5e4
Brianna Rice <brianna.rice@gulllakecs.org>
2019-03-12 12:37:44 -0400

    Break and continue

--- Final_Project.py
+++ Final_Project.py
@@ -1,14 +1,26 @@
 # 3/11/19 Final Project
 # Brianna Rice
 
-print('Pick a number between 1 and 30')
+print('Pick a number between 1 and 30', "\n")
 magic_number = 3
-guess = int(input('Enter a number:'))
+guess = int(input('Enter a number:', "\n"))
 
 while guess != magic_number:
-    print('Guess again')
-    guess = int(input('Enter a number:'))
-print('Now that you got the number I have another task for you.')
+    print('Guess again', "\n")
+    guess = int(input('Enter a number:', "\n"))
+print('Now that you got the number I have another task for you.', "\n")
 
-# I am just going to have them do like an either or thing.
+while True:
+    Mystery = int(input("Enter random number"))
+    if Mystery > 30:
+        print("Nope to big of a number sorry", "\n")
+    elif Mystery < 1:
+        print("No lager number", "\n")
+    else:
+        print("Good choice", "\n")
+        break
+
+
+
+
 
